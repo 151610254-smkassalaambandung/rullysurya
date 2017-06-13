88d689516a264fba670f44b1ccd885970a7e29f6
RafianYoungCreed <151610241@smkassalaambandung.sch.id>
2017-06-13 11:52:51 +0700

    project baru lagi

---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Nama saya Rully Surya
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="Panel Panel-default">
+                <div class="Panel-heading">Hello</div>
+                <div class="Panel-body">
+                    Selamet Aya Didiye
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+@endsection
